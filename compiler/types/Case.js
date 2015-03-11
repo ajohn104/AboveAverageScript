fb309895e3012fffb1f9bab2cf80a6bcb07de7fd
Albert Johnston <ajohn104@lion.lmu.edu>
2015-03-10 17:38:28 -0700

    Clarified macrosyntax and microsyntax slightly

--- compiler/types/Case.js
+++ compiler/types/Case.js
@@ -1,9 +1,25 @@
-// Case            ::= 'case' Indent Block Dedent
+// Case            ::= Newline 'case' Exp18 ':' Indent Block Dedent
 module.exports = {
     is: function() {
         var indexBefore = index;
 
+        if(!expect(Newline)) {
+            index = indexBefore;
+            return false;
+        }
+
         if(parseTokens[index].lexeme !== 'case') {
+            index = indexBefore;
+            return false;
+        } 
+        index++;
+
+        if(!expect(Exp18)) {
+            index = indexBefore;
+            return false;
+        }
+
+        if(parseTokens[index].lexeme !== ':') {
             index = indexBefore;
             return false;
         } 
