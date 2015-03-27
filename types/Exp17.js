18f299677265f9f13cbe23a9a7789f21db9059d5
Albert Johnston <ajohn104@lion.lmu.edu>
2015-03-27 09:19:49 -0700

    Changed indentation rules for dot accessor

--- types/Exp17.js
+++ types/Exp17.js
@@ -2,7 +2,7 @@
 module.exports = {
     is: function(at, next, envir, debug) {
         var indexBefore = envir.index; 
-        var indentedBefore = envir.inIndent;
+        var indentedBefore = envir.inIndented;
         var entity = new Exp17();
         debug("Starting on exp17. envir.index:" + envir.index + ', lexeme: ' + envir.parseTokens[envir.index].lexeme);
         if(!at(envir.Exp18)) {
@@ -11,11 +11,10 @@ module.exports = {
             return false;
         }
         entity.val = envir.last;
-        var indented = false;
         var indexMid = envir.index;
-        while(next(envir.ArrayCont) || next(envir.Call) || next('.') || next(envir.Indent) || (indented && next(envir.Newline))) {
-            if(indented && at(envir.Newline)) {
-                ; // Just suck it up.
+        while(next(envir.ArrayCont) || next(envir.Call) || next('.') || next(envir.Indent) || (envir.inIndented && next(envir.Newline))) {
+            if(envir.inIndented && next(envir.Newline)) {
+                envir.checkIndent();
             } else if(at(envir.ArrayCont)) {
                 entity.accessors.push(envir.last);
             } else if(at(envir.Call)) {
@@ -27,30 +26,21 @@ module.exports = {
                     break;
                 }
                 entity.accessors.push(new DotAccessor(envir.last));
-            } else if(at(envir.Indent)) {
-                if(indented) {
-                    envir.index = indexMid;
-                    debug("Serious indent issue in Exp17");
-                    break;
-                }
-                debug("Exp17: found Indent, grabbing Newline");
-                indented = true;
-                at(envir.Newline);
+            } else if(next(envir.Indent)) {
+                envir.checkIndent();
                 if(!at('.')) {
                     envir.index = indexMid;
+                    envir.inIndented = indentedBefore;
                     break;
                 }
                 if(!at(envir.Exp17)) {
                     envir.index = indexMid;
+                    envir.inIndented = indentedBefore;
                     break;
                 }
                 entity.accessors.push(new DotAccessor(envir.last));
             }
         }
-        if(indented && !at(envir.Dedent)) {
-            envir.index = indexMid;
-        }
-        indented = false;
         envir.last = entity;
         debug("Finalizing exp17 success. envir.index:" + envir.index + ', lexeme: ' + envir.parseTokens[envir.index].lexeme);
         return true;
